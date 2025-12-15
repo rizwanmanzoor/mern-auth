e37105bbde31806569c12ef195653aabcbd17426
rizwan manzoor <rizwanmanzoor200@gmail.com>
2025-12-15 18:50:23 +0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  credentials: true
+}));
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
